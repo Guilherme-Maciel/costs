aa262e1d4b11c51b9083dd103484d0b662c62d36
Guilherme Santos Maciel <guilhermemaciel@plusoft.com>
2022-06-27 16:26:58 -0300

    Estruturação do projeto e ajustes iniciais

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layouts/Container'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to="/">HOME</Link>
+      <Link to="/contact">CONTATO</Link>
+      <Link to="/company">EMPRESA</Link>
+      <Link to="/newproject">NOVO PROJETO</Link>
+      <Switch>
+        <Container customClass="min-height">
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/contact" exact>
+            <Contact/>
+          </Route>
+          <Route path="/company" exact>
+            <Company/>
+          </Route>
+          <Route path="/newproject" exact>
+            <NewProject/>
+          </Route>
+        </Container>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
