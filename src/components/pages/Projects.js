f9deeb99c5487036c5978df7b0266e7988144b0c
Guilherme Santos Maciel <guilhermemaciel@plusoft.com>
2022-07-28 16:57:53 -0300

    Feature: Estruturação do dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import Message from '../layouts/Message'
 import {useLocation} from 'react-router-dom' 
+import styles from './Projects.module.css'
+import Container from '../layouts/Container'
+import LinkButton from '../layouts/LinkButton'
 
 function Projects(){
     const location = useLocation()
@@ -12,9 +15,16 @@ function Projects(){
         type = location.state.type
     }
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
             {message && <Message msg={message} type={type}/>}
+            <Container customClass="start">
+                <p>Projetos...</p>
+
+            </Container>
             
         </div>
     )
